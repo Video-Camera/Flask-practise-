59269aaebace69bb61c49a48437257154b7de3c7
Video-Camera <80290315+Video-Camera@users.noreply.github.com>
2022-11-28 19:21:45 -0800

    Practice http POST and GET request, response. HTML practice

--- test_api.py
+++ test_api.py
@@ -1,10 +1,12 @@
 import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, render_template
 from flask_cors import CORS
+
 
 def connect_to_db():
     connection = sqlite3.connect('mydb.db')
     return connection
+
 
 def insert_user(user):
     inserted_user = {}
@@ -41,13 +43,14 @@ def get_users():
         user = []
     return users
 
+
 def get_user_by_id(user_id):
     user = {}
     try:
         conn = connect_to_db()
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        cur.execute("SELECT * FROM user WHERE UserId = ?", (user_id, ))
+        cur.execute("SELECT * FROM user WHERE UserId = ?", (user_id,))
         row = cur.fetchone()
         user['UserId'] = row['UserId']
         user['username'] = row['username']
@@ -58,12 +61,14 @@ def get_user_by_id(user_id):
 
     return user
 
+
 def update_user(user):
     updated_user = {}
     try:
         conn = connect_to_db()
         cur = conn.cursor()
-        cur.execute("UPDATE user SET Username = ?, Password = ? WHERE UserId = ?", ( user['Username'], user['Password'], user['UserId']))
+        cur.execute("UPDATE user SET Username = ?, Password = ? WHERE UserId = ?",
+                    (user['Username'], user['Password'], user['UserId']))
         conn.commit()
         updated_user = get_user_by_id(user['UserId'])
     except:
@@ -73,13 +78,14 @@ def update_user(user):
         connect_to_db().close()
     return updated_user
 
+
 def delete_user(user_id):
     user_id = int(user_id)
     message = {}
     try:
         conn = connect_to_db()
         cur = conn.cursor()
-        cur.execute("DELETE from user WHERE UserId = ?", (user_id, ))
+        cur.execute("DELETE from user WHERE UserId = ?", (user_id,))
         conn.commit()
         message['status'] = 'USER DELETE'
     except:
@@ -111,6 +117,7 @@ def get_products():
         connect_to_db().close()
     return products
 
+
 def get_product_by_id(product_id):
     product = {}
     try:
@@ -128,13 +135,15 @@ def get_product_by_id(product_id):
         connect_to_db().close()
     return product
 
+
 def add_product(product):
     inserted_product = {}
     try:
         conn = connect_to_db()
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        cur.execute("INSERT INTO product (ProductName, ProductPrice) VALUES (?, ?)", (product["ProductName"], product["ProductPrice"]))
+        cur.execute("INSERT INTO product (ProductName, ProductPrice) VALUES (?, ?)",
+                    (product["ProductName"], product["ProductPrice"]))
         conn.commit()
         inserted_product = get_product_by_id(cur.lastrowid)
     except:
@@ -143,46 +152,151 @@ def add_product(product):
         connect_to_db().close()
     return inserted_product
 
+
+def get_orders_by_id(user_id):
+    user_orders = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("""SELECT norder.OrderCustomerId, norder.OrderProductId, product.ProductName, product.ProductPrice, user.Username
+                                    FROM norder
+                                    INNER JOIN product ON norder.OrderProductId = product.ProductId
+                                    INNER JOIN user on norder.OrderCustomerId = user.UserId
+                                    WHERE norder.OrderCustomerId = ?""", (user_id,))
+        list_of_orders = cur.fetchall()
+        for x in list_of_orders:
+            norder = {}
+            norder["OrderCustomerId"] = x['OrderCustomerId']
+            norder["OrderProductId"] = x['OrderProductId']
+            norder["ProductName"] = x['ProductName']
+            norder["ProductPrice"] = x['ProductPrice']
+            norder["Username"] = x['Username']
+            user_orders.append(norder)
+    except:
+        user_orders = []
+    finally:
+        connect_to_db().close()
+    return user_orders
+
+
+def show_carts():
+    user_carts = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        curr = conn.cursor()
+        curr.execute("""SELECT cart.Customer_id, cart.CartProductId, product.ProductName, product.ProductPrice, user.Username
+                        FROM cart
+                        INNER JOIN product ON cart.CartProductId = product.ProductId
+                        INNER JOIN user ON cart.Customer_id = user.UserId""")
+        list_of_carts = curr.fetchall()
+        for x in list_of_carts:
+            cart = {}
+            cart['Customer_id'] = x['Customer_id']
+            cart['CartProductId'] = x['CartProductId']
+            cart['ProductName'] = x['ProductName']
+            cart['ProductPrice'] = x['ProductPrice']
+            cart['Username'] = x['Username']
+            user_carts.append(cart)
+    except:
+        user_carts = []
+    finally:
+        connect_to_db().close()
+    return user_carts
+
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": '*'}})
+
 
 @app.route("/api/users", methods=["GET"])
 def api_get_users():
     return jsonify(get_users())
 
+
 @app.route('/api/users/<user_id>', methods=['GET'])
 def api_get_user(user_id):
     return jsonify(get_user_by_id(user_id))
 
-@app.route('/api/users/add',  methods = ['POST'])
+
+@app.route('/api/users/add', methods=['POST'])
 def api_add_user():
     user_payload = request.get_json()
     persisted_user = insert_user(user_payload)
     return jsonify(persisted_user)
 
-@app.route('/api/users/update',  methods = ['PUT'])
+
+@app.route('/api/users/update', methods=['PUT'])
 def api_update_user():
     user = request.get_json()
     return jsonify(update_user(user))
 
-@app.route('/api/users/delete/<user_id>', methods = ['DELETE'])
+
+@app.route('/api/users/delete/<user_id>', methods=['DELETE'])
 def api_delete_user(user_id):
     return jsonify(delete_user(user_id))
 
-@app.route("/api/products", methods = ["GET"])
+
+@app.route("/api/products", methods=["GET"])
 def api_get_products():
     return jsonify(get_products())
 
-@app.route("/api/products/<product_id>", methods = ["GET"])
+
+@app.route("/api/products/<product_id>", methods=["GET"])
 def api_get_product(product_id):
     return jsonify(get_product_by_id(product_id))
 
-@app.route("/api/products/add", methods = ["POST"])
+
+@app.route("/api/products/add", methods=["POST"])
 def api_add_product():
     product_payload = request.get_json()
     persisted_product = add_product(product_payload)
     return jsonify(persisted_product)
+
+
+@app.route("/api/orders/<user_id>", methods=['GET'])
+def api_get_orders_by_id(user_id):
+    return jsonify(get_orders_by_id(user_id))
+
+
+@app.route("/api/carts", methods=['GET'])
+def api_get_carts():
+    return jsonify(show_carts())
+
+
+# Create User and add to sql database.
+@app.route("/createuser", methods=["POST", "GET"])
+def create_user():
+    if request.method == "POST":
+        user_name = request.form["Username"]
+        user_password = request.form["Password"]
+        try:
+            conn = connect_to_db()
+            curr = conn.cursor()
+            curr.execute("""INSERT INTO user (Username, Password) VALUES (?, ?)""", (user_name, user_password))
+            conn.commit()
+
+        except:
+            connect_to_db().rollback()
+        finally:
+            connect_to_db().close()
+        return redirect(url_for("user", usr=user_name))
+    else:
+        return render_template('login.html')
+
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1> {usr}</h1>"
+@app.route("/api/home")
+def home():
+    return render_template("index.html")
+
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run(debug=True)
-    app.run() #run app
+    app.run()  # run app
